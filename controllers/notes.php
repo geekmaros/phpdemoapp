54da2aedc46854084e6b3d8f8b2f3acb3706ec38
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-19 00:17:05 +0100

    feat: add query to notes controller

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,3 +1,12 @@
 <?php
-$heading = "Notes";
+$config = require('config.php');
+$db = new Database($config['database']);
+
+
+$heading = "My Notes 📝📝";
+
+
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->fetchAll();
+
+
 require "views/notes.view.php";
