5f4ce3dfe1412c88ad30b46ba654845773e5b53c
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-21 00:17:43 +0100

    feat: update from fetchAll to get

--- controllers/notes.php
+++ controllers/notes.php
@@ -6,7 +6,7 @@ $db = new Database($config['database']);
 $heading = "My Notes 📝📝";
 
 
-$notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->fetchAll();
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->get();
 
 
 require "views/notes.view.php";
