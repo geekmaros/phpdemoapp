64733621c33c9835e2f6d7bc1fc6029d0d01d650
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-21 18:27:24 +0100

    feat: use tailwind form plugins

--- views/partials/head.php
+++ views/partials/head.php
@@ -14,6 +14,6 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
     <title>OUR APP</title>
 </head>
