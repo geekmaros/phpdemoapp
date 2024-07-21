dfa49b41a18af097419f4d19e16509530f33ec9f
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-21 21:37:06 +0100

    feat: handle form submission in note create controller

---
+++ controllers/note-create.php
@@ -0,0 +1,38 @@
+<?php
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = "New Note📝";
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $error = [];
+
+    if(strlen($_POST['body']) === 0 && strlen($_POST['title']) === 0) {
+        $error['body'] = "Body can't be empty";
+        $error['title'] = "Add a Title to your note";
+    }
+
+    if(strlen($_POST['body']) > 1000 && strlen($_POST['title']) > 500) {
+        $error['body'] = "Body can't be more than 1000 words sorry";
+        $error['title'] = "Title can't be more than 500 words sorry";
+    }
+
+
+    $body = htmlspecialchars($_POST['body']);
+    $title = htmlspecialchars($_POST['title']);
+    $user_id = 3;
+
+    if(empty($error)) {
+
+        $db->query("INSERT INTO notes (body, title, user_id) VALUES (:body, :content, :user_id)", [
+            'body' => $body,
+            'content' => $title,
+            'user_id' => $user_id
+        ]);
+    }
+
+//    header('Location: /notes');
+}
+
+require "views/note-create.view.php";
+
