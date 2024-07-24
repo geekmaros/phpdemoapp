33f5874247a28605552dbe9dd08a4992101d3555
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-24 23:39:24 +0100

    feat: replace checkers with validator static function --date "2 days ago"

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,21 +1,23 @@
 <?php
+require('Validator.php');
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = "New Note📝";
 
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $error = [];
 
-    if(strlen($_POST['body']) === 0 && strlen($_POST['title']) === 0) {
+    if(! Validator::string($_POST['body'], 1, 1000) && ! Validator::string($_POST['title'], 1, 500)) {
         $error['body'] = "Body can't be empty";
         $error['title'] = "Add a Title to your note";
     }
-
-    if(strlen($_POST['body']) > 1000 && strlen($_POST['title']) > 500) {
-        $error['body'] = "Body can't be more than 1000 words sorry";
-        $error['title'] = "Title can't be more than 500 words sorry";
-    }
+//
+//    if(strlen($_POST['body']) > 1000 && strlen($_POST['title']) > 500) {
+//        $error['body'] = "Body can't be more than 1000 words sorry";
+//        $error['title'] = "Title can't be more than 500 words sorry";
+//    }
 
 
     $body = htmlspecialchars($_POST['body']);
