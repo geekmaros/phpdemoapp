ef149b60f6570b3a5a05d4c7e8fca5a9e2ce668a
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-28 13:58:43 +0100

    refactor: replace require with base_path

---
+++ controllers/notes/index.php
@@ -0,0 +1,12 @@
+<?php
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 3")->get();
+
+
+view("notes/index.view.php", [
+    'heading' => 'My Notes 📝📝',
+    'notes' => $notes
+]);
