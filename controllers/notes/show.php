cbb9bf78eb80f71a562ed863cfeb6881bdf22b52
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-29 13:59:08 +0100

    refactor: replace require with base_path

---
+++ controllers/notes/show.php
@@ -0,0 +1,19 @@
+<?php
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+
+$heading = "Notes";
+$currentUserId = 3;
+
+
+$id = $_GET['id'];
+
+$note = $db->query("SELECT * FROM notes WHERE id = :id", ['id'  => $id])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show.view.php", [
+    'heading' => "New Note📝",
+    'note' => $note
+]);
