1f8dadd440b76989183cc1e43fd859ef24523329
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-30 14:03:14 +0100

    refactor: move files to notes directory

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php require('partials/head.php') ?>
+<?php require base_path('views/partials/head.php') ?>
 <body>
-<?php require('partials/nav.php') ?>
-<?php require('partials/banner.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
         <div class="mb-10 flex items-center justify-start gap-x-6">
