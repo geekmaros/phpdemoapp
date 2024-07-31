968d8347c10c27fed436372a4a357d8f52cd8a5b
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-07-31 18:37:09 +0100

    refactor: fix assets path

--- views/partials/head.php
+++ views/partials/head.php
@@ -6,10 +6,10 @@
     />
     <meta content="ie=edge" http-equiv="X-UA-Compatible" />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="/public/favicon-48.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../public/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="../../public/favicon-48.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="../../public/favicon-32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../public/favicon-16.png">
     <link rel="manifest" href="/public/manifest.json">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
