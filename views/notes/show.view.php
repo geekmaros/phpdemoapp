e09f39160683e15f40f9e00d7bb9f958079dc92a
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-01 18:38:51 +0100

    feat: add for to delete note in UI

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -14,9 +14,14 @@
 
 
         <div class="bg-white overflow-hidden shadow rounded-lg">
-            <div class="px-4 py-5 sm:px-6">
+            <div class="px-4 py-5 sm:px-6 flex justify-between">
 
                 <h3 class="text-lg font-medium leading-6 text-gray-900"><span class="text-2xl"> 📝</span> <?php echo $note['body']; ?></h3>
+
+                <form action="" method="POST">
+                    <input type="hidden" name="id" value="<?= $note['id']; ?>">
+                    <button class="text-sm hover:bg-red-500 hover:text-white rounded text-red-800 p-2 bg-red-200">🗑️  <span class="ml-1">Delete</span></button>
+                </form>
             </div>
         </div>
     </div>
