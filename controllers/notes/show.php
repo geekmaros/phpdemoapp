09b16429b847983d607abde96842be83ca66ed3b
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-01 18:38:39 +0100

    feat: add delete not functionality to controller

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,19 +4,34 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
+$user_id = 3;
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_POST['id']])->findOrFail();
+
+    authorize($note['user_id'] === $user_id);
+
+   //delete note
+    $db->query("DELETE FROM notes WHERE id = :id AND user_id = :user_id",
+        ['id' => $_POST['id'], 'user_id' => $user_id]);
+
+    header('Location: /notes');
+    exit();
+}else {
 
 
-$heading = "Notes";
-$currentUserId = 3;
+    $heading = "Notes";
 
 
-$id = $_GET['id'];
+    $id = $_GET['id'];
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ['id'  => $id])->findOrFail();
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $id])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $user_id);
 
-view("notes/show.view.php", [
-    'heading' => "New Note📝",
-    'note' => $note
-]);
+    view("notes/show.view.php", [
+        'heading' => "New Note📝",
+        'note' => $note
+    ]);
+}
