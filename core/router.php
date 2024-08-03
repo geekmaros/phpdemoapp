9339c4eb6682ae791cbcd65e9c783661b87c3d13
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2024-08-03 22:49:35 +0100

    refactor: add Router class

--- core/router.php
+++ core/router.php
@@ -1,30 +1,57 @@
 <?php
-// Define routes
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+namespace Core;
+class Router {
+    public $routes = [];
+
+    public function add($method, $uri, $controller)
+    {
+        $this->routes[$uri] = [
+            'uri' => $uri,
+            'method' => $method,
+            'controller' => $controller
+        ];
+    }
 
 
+    public function get($uri, $controller)
+    {
+        $this->add('GET', $uri, $controller);
+    }
+    public function post($uri, $controller)
+    {
+        $this->add('POST', $uri, $controller);
+    }
+    public function delete($uri, $controller)
+    {
+        $this->add('DELETE', $uri, $controller);
+    }
+    public function patch($uri, $controller)
+    {
+        $this->add('PATCH', $uri, $controller);
+    }
+    public function put($uri, $controller)
+    {
+        $this->add('PUT', $uri, $controller);
+    }
 
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if($route['uri'] === $uri && $route['method'] === strtoupper($method)){
+                return require base_path($route['controller']);
+            }
+        }
 
-// function that maps route to controller
-function routeToController($uri, $routes){
-    if(array_key_exists($uri, $routes)){
-        require base_path($routes[$uri]);
-    }else{
-        abort();
+        $this->abort();
+    }
+
+    //// function that aborts the request
+    protected function abort($statusCode = 404)
+    {
+        http_response_code($statusCode);
+
+        require base_path("controllers/{$statusCode}.php");
+
+        die($statusCode);
     }
 }
-
-
-// function that aborts the request
-function abort($statusCode = 404)
-{
-    http_response_code($statusCode);
-
-    require base_path("controllers/{$statusCode}.php");
-
-    die($statusCode);
-}
-
-routeToController($uri, $routes);
-
